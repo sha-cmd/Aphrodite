88e07aef2e9a04a3e9dda6bf2d65aec2bf052aa7
romain Boyrie <romain@boyrie.email>
2022-01-05 13:03:10 +0100

    Commentaires

--- src/multi_plots.py
+++ src/multi_plots.py
@@ -1,14 +1,23 @@
+"""Ce fichier permet d’inspecter dans le répertoire racine du projet,
+en listant tous les répertoires ce terminant par _dvc_plots.
+Ensuite il réalise un dictionnaire contenant
+* le nom du répertoire donc du stage
+* le nom de chaque métrique pour chaque répertoire
+* pour chaque métrique le nom de fichier de train et de validation, assimilé au test dans les graphiques.
+Dans une seconde partie le script crée un nouveau fichier de données mixant le fichier train et le fichier test.
+Enfin il écrit dans un fichier texte toutes les lignes à ajouter dans le stage multi_plots du fichier dvc.yaml"""
+
 import glob
 import os
 import pandas as pd
 import numpy as np
 
+# Liste des répertoires d’intérêts
 dirs = glob.glob('*_dvc_plots')
 dirs_dict = {}
 for dir in dirs:
     metrics_dict = {}
     files = glob.glob(dir + '/*')
-   # print('\n' + dir)
     for file in files:
         train_val_dict = {}
         header = file.split('/')[1][:4]
@@ -19,6 +28,7 @@ for dir in dirs:
                                    'val': dir + '/' + 'val_' + metric_file})
 
             metrics_dict.update({metric: train_val_dict})
+    # Création d’une entrée pour chaque répertoire d’intérêt
     dirs_dict.update({dir: metrics_dict})
 
 if not os.path.isdir('multi_plots'):
@@ -28,10 +38,14 @@ try:
     os.remove('multi_plots/yamlhadoc.txt')
 except FileNotFoundError as e:
     print(e)
+
+# Itération à l’intérieur de notre dictionnaire de dictionnaire
 for key_dir, metrics in dirs_dict.items():
     if not os.path.isdir('multi_plots/' + key_dir):
         os.mkdir('multi_plots/' + key_dir)
     for key_metric, learning in metrics.items():
+        # Nous réalisons des actions de transformation
+        # pour conformer nos données avec le modèle .dvc/plots/multi-plots.json
         df_train = pd.read_json(learning['train'])
         df_train['stage'] = 'train'
         df_val = pd.read_json(learning['val'])
@@ -50,6 +64,7 @@ for key_dir, metrics in dirs_dict.items():
             df.columns = pd.Index(new_cols)
         df = df_train.append(df_val)
         df.to_csv('multi_plots/' + key_dir + '/' + key_metric + '.csv', index_label='index')
+        # Fichier pour rajouter les plots dans dvc.yaml, stage multi_plots
         with open('multi_plots/yamlhadoc.txt', 'a') as f:
             f.writelines(f"- multi_plots/{key_dir}/{key_metric}.csv:\n\
             cache: false\n\
